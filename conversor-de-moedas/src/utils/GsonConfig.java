e026b8bcfc4e809be93d1f7f4bbc858667f55e8a
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 19:33:10 -0300

    Correção classe GsonConfig

--- conversor-de-moedas/src/utils/GsonConfig.java
+++ conversor-de-moedas/src/utils/GsonConfig.java
@@ -8,7 +8,6 @@ public class GsonConfig {
     public static Gson criarGson() {
         return new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setFieldNamingStrategy(FieldNamingPolicy.IDENTITY)
                 .setPrettyPrinting()
                 .create();
     }
