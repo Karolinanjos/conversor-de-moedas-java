3bf9b0ed627b311afef999175f44356b67b90490
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 19:39:06 -0300

    Melhoria classe MoedaExchangeRateAPI

--- conversor-de-moedas/src/models/MoedaExchangeRateAPI.java
+++ conversor-de-moedas/src/models/MoedaExchangeRateAPI.java
@@ -1,6 +1,9 @@
 package models;
 
+import java.util.Map;
+
 public record MoedaExchangeRateAPI(
         String result,
-        String base_code
+        String base_code,
+        Map<String, Double> conversion_rates
 ) {}
