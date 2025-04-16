47303faea8e9ec4ace08a62caa100e44c40843ba
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 19:47:09 -0300

    Melhoria crucial classe Moeda

--- conversor-de-moedas/src/models/Moeda.java
+++ conversor-de-moedas/src/models/Moeda.java
@@ -1,9 +1,11 @@
 package models;
 
+import java.util.Map;
+
 public class Moeda {
     private String result;
     private String base_code;
-    private Double conversion_rates;
+    private Map<String, Double> conversion_rates;
 
     public Moeda(MoedaExchangeRateAPI moedaExchangeRateAPI) {
         this.result = moedaExchangeRateAPI.result();
@@ -11,5 +13,7 @@ public class Moeda {
         this.conversion_rates = moedaExchangeRateAPI.conversion_rates();
     }
 
-
+    public Double getTaxa(String codigoMoedaDestino) {
+        return conversion_rates.get(codigoMoedaDestino);
+    }
 }
