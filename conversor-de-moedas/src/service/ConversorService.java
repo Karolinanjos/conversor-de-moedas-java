4abd75af77d469ce6ba648b9c7eb7e7ae3855a43
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 13:40:27 -0300

    Criação classe CoversorService

---
+++ conversor-de-moedas/src/service/ConversorService.java
@@ -0,0 +1,13 @@
+package service;
+
+import java.io.IOException;
+
+public class ConversorService {
+    private static final String API_KEY = "049f00ec892dac2697d0faa2";
+    private static final String URL_BASE = "https://v6.exchangerate-api.com/v6/" +
+            API_KEY + "/latest/";
+
+    public String buscaMoeda(String moeda) throws IOException, InterruptedException {
+        
+    }
+}
