069cc183702ae7f7b1fd391dfa2d8a419741d764
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 19:41:50 -0300

    Melhoria classe ErroDeConversaoDeMoedaException

--- conversor-de-moedas/src/exceptions/ErroDeConversaoDeMoedaException.java
+++ conversor-de-moedas/src/exceptions/ErroDeConversaoDeMoedaException.java
@@ -1,13 +1,7 @@
 package exceptions;
 
 public class ErroDeConversaoDeMoedaException extends RuntimeException {
-    private String mensagem;
-
     public ErroDeConversaoDeMoedaException(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    public String getMensagem() {
-        return this.mensagem;
+        super(mensagem);
     }
 }
--- conversor-de-moedas/src/models/Moeda.java
+++ conversor-de-moedas/src/models/Moeda.java
@@ -3,17 +3,13 @@ package models;
 public class Moeda {
     private String result;
     private String base_code;
+    private Double conversion_rates;
 
     public Moeda(MoedaExchangeRateAPI moedaExchangeRateAPI) {
         this.result = moedaExchangeRateAPI.result();
         this.base_code = moedaExchangeRateAPI.base_code();
+        this.conversion_rates = moedaExchangeRateAPI.conversion_rates();
     }
 
-    @Override
-    public String toString() {
-        return "Moeda{" +
-                "result='" + result + '\'' +
-                ", base_code='" + base_code + '\'' +
-                '}';
-    }
+
 }
