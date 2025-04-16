ff3cbfe180443906d2116da8601eea8f1c89abc8
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 19:00:30 -0300

    Implementação MoedaController

--- conversor-de-moedas/src/application/Main.java
+++ conversor-de-moedas/src/application/Main.java
@@ -1,12 +1,19 @@
 package application;
 
+import com.google.gson.Gson;
+import controller.MoedaController;
+import service.ConversorService;
+import utils.GsonConfig;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String moeda = "";
 
-
+        Gson gson = GsonConfig.criarGson();
+        MoedaController controller = new MoedaController(new ConversorService(), gson);
 
         scanner.close();
     }
--- conversor-de-moedas/src/controller/MoedaController.java
+++ conversor-de-moedas/src/controller/MoedaController.java
@@ -1,5 +1,26 @@
 package controller;
 
-public class MoedaController {
+import com.google.gson.Gson;
+import exceptions.ErroDeConversaoDeMoedaException;
+import models.Moeda;
+import models.MoedaExchangeRateAPI;
+import service.ConversorService;
 
+import java.io.IOException;
+
+public class MoedaController {
+    private final ConversorService service;
+    private final Gson gson;
+
+    public MoedaController(ConversorService service, Gson gson) {
+        this.service = service;
+        this.gson = gson;
+    }
+
+    public Moeda buscaValorMoeda(String valor) throws
+            IOException, InterruptedException, ErroDeConversaoDeMoedaException {
+        String json = service.buscaMoeda(valor);
+        MoedaExchangeRateAPI moedaExchangeRateAPI = gson.fromJson(json, MoedaExchangeRateAPI.class);
+        return new Moeda(moedaExchangeRateAPI);
+    }
 }
