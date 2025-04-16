fa3cec5f31f45ad36cc2ae6fdd06a22d7f7ce64e
renancvitor <renan.vitor.cm@gmail.com>
2025-04-16 08:05:56 -0300

    Criação do GsonConfig

---
+++ conversor-de-moedas/src/utils/GsonConfig.java
@@ -0,0 +1,15 @@
+package utils;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonConfig {
+    public static Gson criarGson() {
+        return new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setFieldNamingStrategy(FieldNamingPolicy.IDENTITY)
+                .setPrettyPrinting()
+                .create();
+    }
+}
